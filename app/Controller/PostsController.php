4f5f11e63af9b34bfebcdd4003213ccfa77c00ae
Valerie Cayo <vc74377n@pace.edu>
2013-09-20 10:17:55 -0400

    Added an edit posts method to the blog 0h5m

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -30,5 +30,29 @@ class PostsController extends AppController {
         }
     }
 
+    public function edit($id = null) {
+    if (!$id) {
+        throw new NotFoundException(__('Invalid post'));
+    }
+
+    $post = $this->Post->findById($id);
+    if (!$post) {
+        throw new NotFoundException(__('Invalid post'));
+    }
+
+    if ($this->request->is('post') || $this->request->is('put')) {
+        $this->Post->id = $id;
+        if ($this->Post->save($this->request->data)) {
+            $this->Session->setFlash(__('Your post has been updated.'));
+            return $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Unable to update your post.'));
+    }
+
+    if (!$this->request->data) {
+        $this->request->data = $post;
+    }
+}
+
 }
 ?>
