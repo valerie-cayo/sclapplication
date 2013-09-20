a5da3c193e1128ed53a1b2a778772a223dcc093f
Valerie Cayo <vc74377n@pace.edu>
2013-09-20 10:32:05 -0400

    Added a delete postsfunction and rerouted to the blogs homepage. 0h11m

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -31,28 +31,39 @@ class PostsController extends AppController {
     }
 
     public function edit($id = null) {
-    if (!$id) {
-        throw new NotFoundException(__('Invalid post'));
-    }
+	    if (!$id) {
+	        throw new NotFoundException(__('Invalid post'));
+	    }
 
-    $post = $this->Post->findById($id);
-    if (!$post) {
-        throw new NotFoundException(__('Invalid post'));
-    }
+	    $post = $this->Post->findById($id);
+	    if (!$post) {
+	        throw new NotFoundException(__('Invalid post'));
+	    }
 
-    if ($this->request->is('post') || $this->request->is('put')) {
-        $this->Post->id = $id;
-        if ($this->Post->save($this->request->data)) {
-            $this->Session->setFlash(__('Your post has been updated.'));
-            return $this->redirect(array('action' => 'index'));
-        }
-        $this->Session->setFlash(__('Unable to update your post.'));
-    }
+	    if ($this->request->is('post') || $this->request->is('put')) {
+	        $this->Post->id = $id;
+	        if ($this->Post->save($this->request->data)) {
+	            $this->Session->setFlash(__('Your post has been updated.'));
+	            return $this->redirect(array('action' => 'index'));
+	        }
+	        $this->Session->setFlash(__('Unable to update your post.'));
+	    }
 
-    if (!$this->request->data) {
-        $this->request->data = $post;
-    }
+	    if (!$this->request->data) {
+	        $this->request->data = $post;
+	    }
+	}
+
+    public function delete($id) {
+	    if ($this->request->is('get')) {
+	        throw new MethodNotAllowedException();
+	    }
+
+	    if ($this->Post->delete($id)) {
+	        $this->Session->setFlash(__('The post with id: %s has been deleted.', h($id)));
+	        return $this->redirect(array('action' => 'index'));
+	    }
+	}
 }
 
-}
 ?>
